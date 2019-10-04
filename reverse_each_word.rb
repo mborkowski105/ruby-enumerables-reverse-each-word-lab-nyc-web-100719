5bcd1dc7c7dc3a9655e8ecfea3851658e8af1923
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-04 01:43:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word(phrase)
   new_phrase = phrase.split(" ")
-  new_phrase = new_phrase.each do {|n| n.reverse }
+  new_phrase = new_phrase.each do { |n| n.reverse }
   
   return new_phrase
 end
 
 [1, 2, 3].each { |n| puts "Current number is: #{n}" }
+"olleH ,ereht dna woh era ?uoy"
