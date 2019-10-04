40b1811917dba2aa1dd645e0792d767de88c685a
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-04 01:48:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(phrase)
   new_phrase = phrase.split(" ")
-  new_phrase = new_phrase.each do { |n| n.reverse }
+  new_phrase = new_phrase.each { |n| n.reverse }
   new_phrase = new_phrase.join(" ")
   
   return new_phrase
